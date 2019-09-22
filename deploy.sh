9e3964254f191218ab5172e6db2fdca66213cfca
mdeblaauw <mdeblaauw@mobiquityinc.com>
2019-09-22 10:27:02 +0200

    update yaml and bash file

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo Hello world!
+aws cloudformation package --template-file infrastructure.yaml --s3-bucket blaauwtestbucket --output-template-file infrastructure.packaged.yaml
+aws cloudformation deploy --template-file infrastructure.packaged.yaml --stack-name logoApplication --capabilities "CAPABILITY_IAM"
+
