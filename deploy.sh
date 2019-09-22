aa99a1329da24d371e02ed84cc42611397bdff6e
mdeblaauw <mdeblaauw@mobiquityinc.com>
2019-09-22 11:37:55 +0200

    update bash file

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-aws cloudformation package --template-file infrastructure.yaml --s3-bucket blaauwtestbucket --output-template-file infrastructure.packaged.yaml
-aws cloudformation deploy --template-file infrastructure.packaged.yaml --stack-name logoApplication --capabilities "CAPABILITY_IAM"
+input1="$1"
+bucketName=blaauwtestbucket
+stackName=logoApplication
+templateFile=infrastructure.yaml
+outTemplateFile=infrastructure.packaged.yaml
+
+if [ $input1 == "update" ]; then
+    aws cloudformation update-stack --stack-name $stackName
+elif [ $input1 == "delete" ]; then
+    echo deleting stack
+    aws cloudformation delete-stack --stack-name $stackName
+    echo deleting succeeded
+else
+    aws cloudformation package --template-file $templateFile --s3-bucket $bucketName --output-template-file $outTemplateFile
+    aws cloudformation deploy --template-file $outTemplateFile --stack-name $stackName --capabilities "CAPABILITY_IAM"
+fi
+
+
 
