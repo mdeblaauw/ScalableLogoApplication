449f08e1004d6c30010893d578aca37859d408db
mdeblaauw <mdeblaauw@mobiquityinc.com>
2019-09-25 19:53:59 +0200

    add alot of stuff

---
+++ lambda_functions/model_function/lambda_files/lambda_function.py
@@ -0,0 +1,124 @@
+import json
+import boto3
+from io import BytesIO
+import os
+import time
+from torchvision import transforms
+from PIL import Image
+from resnet_model import ResNet18
+from protonet import *
+
+s3 = boto3.client('s3')
+dynamodb = boto3.client('dynamodb')
+embedding_dim = 256
+distance = "gaussian"
+
+def lambda_handler(event, context):
+    load_and_store_files()
+    print(event)
+    query,file_key = get_image(event)
+    model = install_model()
+    values = make_inference(query, model)
+    store_values(values, file_key)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
+
+def load_and_store_files():
+    weight_path = "/tmp/model-weights"
+    prepare_path = "/tmp/preparation-files"
+    if not os.path.isdir(weight_path):
+        os.mkdir(weight_path)
+        content = s3.list_objects_v2(Bucket=os.environ['preparation_bucket'], Prefix='model-weights')
+        key = content['Contents'][0]['Key']
+        print('/tmp/' + key)
+        s3.download_file(os.environ['preparation_bucket'], key, '/tmp/' + key)
+        print('no weights')
+    if not os.path.isdir(prepare_path):
+        os.mkdir(prepare_path)
+        print('no preparation files')
+        s3.download_file(os.environ['preparation_bucket'], 'preparation-files/S.pt', prepare_path + '/S.pt')
+        s3.download_file(os.environ['preparation_bucket'], 'preparation-files/prototypes.pt',
+                         prepare_path + '/prototypes.pt')
+        s3.download_file(os.environ['preparation_bucket'], 'preparation-files/reverse_class_mapping.json',
+                         prepare_path + '/reverse_class_mapping.json')
+
+
+def get_image(event):
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        file_key = record['s3']['object']['key']
+        print(bucket)
+        print(file_key)
+
+    with BytesIO() as files:
+        s3.download_fileobj(bucket, file_key, files)
+        query = Image.open(files).convert("RGB")
+
+        return query,file_key
+
+
+def install_model():
+    model_name = os.listdir("/tmp/model-weights")[0]
+    model = ResNet18(flatten=True).to("cpu", dtype=torch.float)
+    model.load_state_dict(torch.load(os.path.join("/tmp/model-weights", model_name), map_location="cpu"))
+    model.eval()
+    return model
+
+
+def make_inference(query, model):
+    prototypes = torch.load('/tmp/preparation-files/prototypes.pt')
+    S = torch.load('/tmp/preparation-files/S.pt')
+    with open('/tmp/preparation-files/reverse_class_mapping.json') as json_file:
+        reverse_class_mapping = json.load(json_file)
+
+    processed_query = preprocess_image(query)
+
+    query_output = model(processed_query)
+    query_embedding, _ = torch.split(query_output, [embedding_dim, embedding_dim], dim=1)
+
+    distances = pairwise_distances(query_embedding, prototypes, distance, S)
+    y_pred = (-distances).softmax(dim=1)
+
+    min_dist = torch.min(distances).data.numpy()
+    max_prob = torch.max(y_pred).data.numpy()
+    prob_label = reverse_class_mapping[str(torch.argmax(y_pred).data.numpy())]
+    dist_label = reverse_class_mapping[str(torch.argmin(distances).data.numpy())]
+
+    print('min distance:', min_dist)
+    print('max prob:', max_prob)
+    print('prob label:', prob_label)
+
+    return [min_dist, max_prob, prob_label, dist_label]
+
+
+def preprocess_image(query):
+    transform = transforms.Compose([
+        transforms.Resize((60, 60)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                             std=[0.5, 0.5, 0.5])
+    ])
+
+    return torch.unsqueeze(transform(query), 0)
+
+
+def store_values(values,file_key):
+
+    current_milli_time = lambda: int(round(time.time() * 1000))
+    current_time = current_milli_time()
+
+    dynamodb.put_item(
+        TableName = os.environ['TableName'],
+        Item = {
+            "ImageId": {'S': file_key.split('.')[0]},
+            "Timestamp": {'N': str(current_time)},
+            "MinDist": {'N': str(values[0])},
+            "MaxProb": {'N': str(values[1])},
+            "ProbLabel": {'S': values[2]},
+            "DistLabel": {'S': values[3]}
+        }
+    )
--- lambda_functions/model_function/lambda_files/protonet.py
+++ lambda_functions/model_function/lambda_files/protonet.py
@@ -1,10 +1,12 @@
 import torch
 import torch.nn.functional as F
 
-def calculate_inverse_covariance_matrix(raw_covariance_matrix, offset = 1.0):
-    #offset = 1.0
+
+def calculate_inverse_covariance_matrix(raw_covariance_matrix, offset=1.0):
+    # offset = 1.0
     inv_covariance_matrix = offset + F.softplus(raw_covariance_matrix, beta=1, threshold=20)
     return inv_covariance_matrix
+
 
 def compute_matrix(inv_covariance_matrix: torch.Tensor, k: int, n: int, embedding_dim: int) -> torch.Tensor:
     """Compute class prototypes from support samples.
@@ -18,19 +20,20 @@ def compute_matrix(inv_covariance_matrix: torch.Tensor, k: int, n: int, embeddin
     """
     # Reshape so the first dimension indexes by class then take the mean
     # along that dimension to generate the "prototypes" for each class
-    
-    #support_unsqueeze = support.reshape(k, n, -1)
-    
-    inv_covariance_exp = inv_covariance_matrix.expand([-1,embedding_dim])
+
+    # support_unsqueeze = support.reshape(k, n, -1)
+
+    inv_covariance_exp = inv_covariance_matrix.expand([-1, embedding_dim])
     inv_covariance_unsqueeze = inv_covariance_exp.reshape(k, n, -1)
-    
-    #sum1 = (support_unsqueeze * inv_covariance_unsqueeze).sum(dim=1)
-    
+
+    # sum1 = (support_unsqueeze * inv_covariance_unsqueeze).sum(dim=1)
+
     S = inv_covariance_unsqueeze.sum(dim=1)
-    
-    #class_prototypes = sum1/S
-        
+
+    # class_prototypes = sum1/S
+
     return S
+
 
 def compute_prototypes(support: torch.Tensor, k: int, n: int) -> torch.Tensor:
     """Compute class prototypes from support samples.
@@ -47,10 +50,11 @@ def compute_prototypes(support: torch.Tensor, k: int, n: int) -> torch.Tensor:
     class_prototypes = support.reshape(k, n, -1).mean(dim=1)
     return class_prototypes
 
+
 def pairwise_distances(x: torch.Tensor,
                        y: torch.Tensor,
                        matching_fn: str,
-                       S = None) -> torch.Tensor:
+                       S=None) -> torch.Tensor:
     """Efficiently calculate pairwise distances (or other similarity scores) between
     two sets of samples.
     # Arguments
@@ -69,15 +73,15 @@ def pairwise_distances(x: torch.Tensor,
         return distances
     elif matching_fn == 'gaussian':
         distances = ((
-                x.unsqueeze(1).expand(n_x, n_y, -1) -
-                y.unsqueeze(0).expand(n_x, n_y, -1)
-        ).pow(2) * S.unsqueeze(0).expand(n_x, n_y,-1)).sum(dim=2)
-    
+                             x.unsqueeze(1).expand(n_x, n_y, -1) -
+                             y.unsqueeze(0).expand(n_x, n_y, -1)
+                     ).pow(2) * S.unsqueeze(0).expand(n_x, n_y, -1)).sum(dim=2)
+
         distances = distances.sqrt()
         return distances
     elif matching_fn == 'gaussian_v2':
         difference = x.unsqueeze(1).expand(n_x, n_y, -1) - y.unsqueeze(0).expand(n_x, n_y, -1)
-        difference_two = torch.matmul(S.unsqueeze(0).expand(n_x, n_y,-1,-1), difference.unsqueeze(3))
+        difference_two = torch.matmul(S.unsqueeze(0).expand(n_x, n_y, -1, -1), difference.unsqueeze(3))
         distances = torch.matmul(difference.unsqueeze(2), difference_two).squeeze()
         distances = distances.sqrt()
         return distances
@@ -96,4 +100,4 @@ def pairwise_distances(x: torch.Tensor,
 
         return -(expanded_x * expanded_y).sum(dim=2)
     else:
-        raise(ValueError('Unsupported similarity function'))
+        raise (ValueError('Unsupported similarity function'))
