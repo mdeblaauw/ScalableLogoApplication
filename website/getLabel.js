dc7666649c75cd7373cde4be7c9583f1842bb326
mdeblaauw <mdeblaauw@mobiquityinc.com>
2019-09-28 12:42:10 +0200

    removed some bugs

--- website/getLabel.js
+++ website/getLabel.js
@@ -1,14 +1,36 @@
+const delayMs = 2000;
+
 async function getLabel(imageId) {
     console.log(imageId);
 
-    const response = await axios({
-        method: 'GET',
-        url: API_URL + "/get-label",
-        //params: {imageId: this.imageId}
-        params: {imageId: "8cb23bb5-e8b6-4e4e-a226-58c186488711"}
-    });
+    const delay = function(ms) {
+        return new Promise(function(resolve) {
+            setTimeout(resolve, ms);
+         })
+    };
 
-    console.log(response);
-    console.log(response.data.ProbLabel.S);
+     async function getImageLabel() {
+        await delay(delayMs);
+        let count = 0;
+        while (count < 7){
+            const response = await axios({
+                method: 'GET',
+                url: API_URL + "/get-label",
+                params: {imageId: imageId}
+            });
+            if(response.data) {
+                document.getElementById("result").innerHTML = response.data.ProbLabel.S
+                return console.log(response.data.ProbLabel.S);
+            }
+            console.log(count);
+            count++
+            await delay(delayMs);
+        }
+    }
+
+    getImageLabel();
+
+    //console.log(answer);
+    //console.log(answer.data.ProbLabel.S);
 
 }
