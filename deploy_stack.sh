73586789138458e9fe460c94a457278aaafbcc11
mdeblaauw <mdeblaauw@mobiquityinc.com>
2019-10-27 11:35:21 +0100

    add dev prod versioning with domainname in prod

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -2,29 +2,22 @@
 
 input1="$1"
 bucketName=logoapplication-lambdafiles-34fl9as0
-stackName=logoApplication
+stackName=logoApplication-${input1}
 templateFile=infrastructure.yaml
 outTemplateFile=infrastructure.packaged.yaml
 
-if [ $input1 == "update" ]; then
-    aws cloudformation update-stack --stack-name $stackName
-elif [ $input1 == "delete" ]; then
-    echo deleting stack
-    aws cloudformation delete-stack --stack-name $stackName
-    echo deleting succeeded
-else
-    aws cloudformation package --template-file $templateFile --s3-bucket $bucketName --output-template-file $outTemplateFile
-    aws cloudformation deploy --template-file $outTemplateFile --stack-name $stackName --capabilities "CAPABILITY_IAM"
+aws cloudformation package --template-file $templateFile --s3-bucket $bucketName --output-template-file $outTemplateFile
+aws cloudformation deploy --template-file $outTemplateFile --stack-name $stackName --parameter-overrides EnvType=$input1 --capabilities "CAPABILITY_IAM"
 
-    echo This is the get image url API
-    aws cloudformation list-exports --query "Exports[?Name==\`GetApiURL\`].Value"
+echo This is the get image url API
+aws cloudformation list-exports --query "Exports[?Name==\`GetApiURL-${input1}\`].Value"
 
-    echo This is the website url
-    aws cloudformation list-exports --query "Exports[?Name==\`WebsiteURL\`].Value"
+echo This is the website url
+aws cloudformation list-exports --query "Exports[?Name==\`WebsiteURL-${input1}\`].Value"
 
-    echo This is the name of the preparation bucket
-    aws cloudformation list-exports --query "Exports[?Name==\`PrepBucketName\`].Value"
-fi
+echo This is the name of the preparation bucket
+aws cloudformation list-exports --query "Exports[?Name==\`PrepBucketName-${input1}\`].Value"
+
 
 
 
--- deploy_website.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo Upload webiste to logoapplication-website-39bk35l8 bucket.
-aws s3 sync website/ s3://logoapplication-website-39bk35l8/
-echo Upload finished.
