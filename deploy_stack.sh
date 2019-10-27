621b68c2a30e9a4a88c77d21ad88a55f1e7f1917
mdeblaauw <mdeblaauw@mobiquityinc.com>
2019-10-27 15:22:51 +0100

    add deletionPolicy to prod

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 input1="$1"
+input2="$2"
 bucketName=logoapplication-lambdafiles-34fl9as0
 stackName=logoApplication-${input1}
 templateFile=infrastructure.yaml
 outTemplateFile=infrastructure.packaged.yaml
 
 aws cloudformation package --template-file $templateFile --s3-bucket $bucketName --output-template-file $outTemplateFile
-aws cloudformation deploy --template-file $outTemplateFile --stack-name $stackName --parameter-overrides EnvType=$input1 --capabilities "CAPABILITY_IAM"
+aws cloudformation deploy --template-file $outTemplateFile --stack-name $stackName --parameter-overrides EnvType=$input1 ProdVersion=$input2 --capabilities "CAPABILITY_IAM"
 
 echo This is the get image url API
 aws cloudformation list-exports --query "Exports[?Name==\`GetApiURL-${input1}\`].Value"
